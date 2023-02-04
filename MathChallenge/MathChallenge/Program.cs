75edbd97f20aa5e2d7f9ad7756b01d9dd0c41e3f
Lilian Tan <lilytan6007@gmail.com>
2023-02-03 21:29:40 -0800

    Add Math Challenges from module 7

---
+++ MathChallenge/MathChallenge/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathChallenge
+{
+    class Program
+    {
+        static void Main()
+        {
+
+
+
+            // Addition
+            //int num1 = 50;
+            //int num2 = 90;
+            //int total = num1 + num2;
+            //Console.WriteLine(total);
+            //Console.ReadLine();
+
+            // Subtraction
+            //int num3 = 32;
+            //int num4 = 5;
+            //int difference = num3 - num4;
+            //console.writeline(difference);
+            //console.readline();
+
+            // Multiplication
+            //int num5 = 5;
+            //int num6 = 10;
+            //int multiply = num5 * num6;
+            //Console.WriteLine(multiply);
+            //Console.ReadLine();
+
+            // Division 
+            int num7 = 70;
+            int num8 = 2;
+            int divide = num7 / num8;
+            Console.WriteLine(divide);
+            Console.ReadLine();
+
+            // Math String Challenge
+            string firstName = "Lilian";
+            string lastName = "T";
+            Console.WriteLine(firstName + lastName);
+            Console.ReadLine();
+
+
+            
+        }
+    }
+}
