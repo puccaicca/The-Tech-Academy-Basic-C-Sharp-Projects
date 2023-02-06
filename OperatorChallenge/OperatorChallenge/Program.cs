025835c7c877757ba49ca1f3634c9bf14a03a1c3
Lilian Tan <lilytan6007@gmail.com>
2023-02-05 21:07:36 -0800

    Add != operator challenge

---
+++ OperatorChallenge/OperatorChallenge/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Operator Challenge using the != operator (not equal)
+            Console.WriteLine("Write your name please");
+            string name = Console.ReadLine();
+            
+
+            if (name != "Lilian")
+            {
+                Console.WriteLine("Why you here");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Welcome back, Lilian!");
+                Console.ReadLine();
+            }
+        }
+    }
+}
