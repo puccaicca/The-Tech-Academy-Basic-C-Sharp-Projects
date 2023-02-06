d00dc5ba8f10a55384299bb2b9491bae0a194457
Lilian Tan <lilytan6007@gmail.com>
2023-02-05 21:21:12 -0800

    Add Console Output Challange <= and >=

---
+++ ConsoleOutputChallenge/ConsoleOutputChallenge/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleOutputChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input your age");
+            string age = Console.ReadLine();
+            int yourage = Convert.ToInt32(age);
+            Console.WriteLine("You're " + yourage + " years old");
+            Console.ReadLine();
+
+            if (yourage <= 20)
+            {
+                Console.WriteLine("You may not purchase that beer");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Yes, please buy this beer");
+                Console.ReadLine();
+            }
+
+
+            Console.WriteLine("Please input your age");
+            string age2 = Console.ReadLine();
+            int yourage2 = Convert.ToInt32(age2);
+            Console.WriteLine("You're " + yourage2 + " years old");
+            Console.ReadLine();
+
+            if (yourage2 >= 60)
+            {
+                Console.WriteLine("Be careful of your health before you drink this beer!");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Enjoy this beer!");
+                Console.ReadLine();
+            }
+        }
+    }
+}
