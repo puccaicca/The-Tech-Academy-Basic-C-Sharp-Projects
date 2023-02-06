f3d95c2d0b02f2cff063ca49e7678359557669ab
Lilian Tan <lilytan6007@gmail.com>
2023-02-05 21:34:27 -0800

    Add ternary operator challange

---
+++ TernaryOperatorChallenge/TernaryOperatorChallenge/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TernaryOperatorChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 77;
+            int num2 = 12;
+
+            string result = num1 > num2 ? "num 1 is greater than num2" : "num1 is not greater than num2";
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
