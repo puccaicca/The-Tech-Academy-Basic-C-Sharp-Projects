0cfb7faa66ef9c21b424d289499d12ee8780ea88
Lilian Tan <lilytan6007@gmail.com>
2023-02-07 21:46:10 -0800

    Add switch challenge

---
+++ SwitchChallenge/SwitchChallenge/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwitchChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int day = 4;
+            switch (day)
+            {
+                case 6:
+                    Console.WriteLine("Today is Saturday.");
+                    break;
+                case 7:
+                    Console.WriteLine("Today is Sunday.");
+                    break;
+                default:
+                    Console.WriteLine("Looking forward to the weekend.");
+                    Console.ReadLine();
+                    break;
+                    //Outputs = looking forward to the weekend
+            }
+        }
+    }
+}
