1b05ae8625545184dbc83b938592b6eeb63bc8b1
Lilian Tan <lilytan6007@gmail.com>
2023-02-07 21:29:03 -0800

    Update program.cs

--- BranchingAssignmentSubmission/BranchingAssignmentSubmission/Program.cs
+++ BranchingAssignmentSubmission/BranchingAssignmentSubmission/Program.cs
@@ -11,37 +11,53 @@ namespace BranchingAssignmentSubmission
         static void Main(string[] args)
         {
             // Title
-            Console.WriteLine("Welcome to Package Express. Please follow the instruction below.");
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
             Console.ReadLine();
 
             // User inputs weight > converts to int 
-            Console.WriteLine("Please enter the package weight");
+            Console.WriteLine("Please enter the package weight:");
             int weight = Convert.ToInt32(Console.ReadLine());
-            string weightResult = weight < 50 ? "Your package is not too heavy." : "Package too heavy to be shipped via Package Express. Have a good day."; // if weight is over 50 the program ends 
-            Console.WriteLine(weightResult);
-            Console.ReadLine();
 
-            // asks for the width > converts it to int 
-            Console.WriteLine("Please enter the package width:");
-            int width = Convert.ToInt32(Console.ReadLine());
+            // if weight > 50, the program ends with the message
+            if (weight > 50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express");
+                Console.ReadLine();
+            }
 
-            // asks for the height > converts it to int 
-            Console.WriteLine("Please enter the package height:");
-            int height = Convert.ToInt32(Console.ReadLine());
+            // program continues
+            else
+            {
+                // asks user for package width > converts to int
+                Console.WriteLine("Please enter the package width:");
+                int width = Convert.ToInt32(Console.ReadLine());
 
-            // asks for the length > converts it to int 
-            Console.WriteLine("Please enter the package length:");
-            int length = Convert.ToInt32(Console.ReadLine());
+                //asks user for package height > converts to int
+                Console.WriteLine("Please enter the package height:");
+                int height = Convert.ToInt32(Console.ReadLine());
 
-            // calculates the dimensions, program ends if total is over 50
-            string dimensions = width + height + length < 50 ? "Your package is shippable." : "Package too big to be shipped via Package Express.";
-            Console.WriteLine(dimensions);
-            Console.ReadLine();
+                // asks for package length > converts it to int 
+                Console.WriteLine("Please enter the package length:");
+                int length = Convert.ToInt32(Console.ReadLine());
 
-            // shipping calculation : mulitply the dimensions together, multiply product by weight, divide outcome by 100
-            decimal shipping = (height * width * length) * weight / 100;  
-            Console.WriteLine("Your estimated total for shipping this package is: $" + shipping + "\nThank you, have a great day!");
-            Console.ReadLine();
+                // calculates the dimensions
+                int dimensions = width + height + length;
+
+                // if dimensions is over 50, the program ends
+                if (dimensions > 50)
+                {
+                    Console.WriteLine("Package too big to be shipped via Package Express.");
+                    Console.ReadLine();
+                }
+                // program continues
+                else
+                {
+                    // shipping calculation : mulitply the dimensions together, multiply product by weight, divide outcome by 100
+                    decimal shipping = (height * width * length) * weight / 100.00m;
+                    Console.WriteLine("Your estimated total for shipping this package is: $" + shipping + "\nThank you, have a great day!");
+                    Console.ReadLine();
+                }
+            }
 
         }
     }
