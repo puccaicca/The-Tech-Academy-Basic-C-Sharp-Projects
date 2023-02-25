400f1a7551354982efbdd0f74eaa3507ca3ecc44
Lilian Tan <lilytan6007@gmail.com>
2023-02-24 19:23:28 -0800

    Add and update parts 4-6

--- PART 2/Module2_ConsoleAppAssignment/Module2_ConsoleAppAssignment/Program.cs
+++ PART 2/Module2_ConsoleAppAssignment/Module2_ConsoleAppAssignment/Program.cs
@@ -13,22 +13,58 @@ namespace Module2_ConsoleAppAssignment
             // PART 1
 
             // Array of strings (one-dimensional)
-            string[] colors = { "Orange", "Yellow", "Green", "Blue", "Purple" };
+            //string[] colors = { "Orange", "Yellow", "Green", "Blue", "Purple" };
 
-            // Asks user to input some text
-            Console.WriteLine("Please add a color to the array");
-            string addColor = Console.ReadLine();
+            //// Asks user to input some text
+            //Console.WriteLine("Please add a color to the array");
+            //string addColor = Console.ReadLine();
 
-            // Loop that iterates through each string and adds the user's text input to the end of each string
-            for (int i = 0; i < colors.Length; i++)
+            //// Loop that iterates through each string and adds the user's text input to the end of each string
+            //for (int i = 0; i < colors.Length; i++)
+            //{
+            //    colors[i] = colors[i] + addColor;
+            //}
+
+            //// Second loop prints all strings in the array one at a time
+            //foreach (string color in colors)
+            //{
+            //    Console.WriteLine(color);
+            //}
+            //Console.ReadLine();
+
+            string[] wordstart =
             {
-                colors[i] = colors[i] + addColor;
+                "Welcome to The ",
+                "I'm Principal, ",
+                "Our goal is to provide our students with a "
+            };
+            string[] wordend =
+            {
+                " Academy.",
+                ".",
+                " learning experience."
+            };
+
+            List<string> responses = new List<string>();
+            //Ask the user to input some text.
+            Console.WriteLine("Please enter a noun:");
+            responses.Add(Console.ReadLine());
+            Console.WriteLine("Please enter the name of someone infamous:");
+            responses.Add(Console.ReadLine());
+            Console.WriteLine("Please enter an adjective");
+            responses.Add(Console.ReadLine());
+            //Create a loop that goes through each string in the Array, adding the user's text to the string.
+            for (int i = 0; i < wordstart.Length; i++)
+            {
+                wordstart[i] += responses[i];
+                Console.WriteLine(wordstart[i] + wordend[i]);
             }
-
-            // Second loop prints all strings in the array one at a time
-            foreach (string color in colors)
+            Console.ReadLine();
+            Console.WriteLine("The strings we used:");
+            //Then crate a loop that prints off each string in the Array on a seperate line.
+            for (int i = 0; i < wordstart.Length; i++)
             {
-                Console.WriteLine(color);
+                Console.WriteLine(wordstart[i] + wordend[i]);
             }
             Console.ReadLine();
 
@@ -76,36 +112,91 @@ namespace Module2_ConsoleAppAssignment
             // PART 4
 
             // A list of strings where each item in the list is unique
-            List<string> furnitures = new List<string>() { "couch", "desk", "bed", "dresser", "coat rack", "mirror", "chair" };
-
-            // Asks user to input text to search for in the list
-            Console.WriteLine("Which type of furniture are you looking for?");
+            List<string> teams = new List<string>() {  "Bucks", "Raptors", "Celtics", "Heat", "Pacers", "76ers", "Nets", "Magic",
+                "Wizards", "Hornets", "Bulls", "Knicks", "Pistons", "Hawks", "Cavaliers" };
+            Console.WriteLine("NBA Eastern Conference Standings");
+            //Asks the user to input text to search for in the List
+            Console.WriteLine("Enter team name:");
+            int standing = 0;
             bool isValid = false;
             int index = 0;
-            
-
-
-            // A loop that iterates through the list and then displays the index of the list item that contains matching text on the screen
+            //Create a loop that ierates through the list and then displays the index of the array that contains matching text on the screen.
             while (!isValid)
             {
                 int i2 = 0;
-                string askFurniture = Console.ReadLine();
-                foreach (string furniture in furnitures)
+                string teamrequest = Console.ReadLine();
+                foreach (string team in teams)
                 {
-                    if (askFurniture == furniture)
+                    if (teamrequest == team)
                     {
+                        standing = i2 + 1;
                         index = i2;
                     }
                     i2++;
-             
-                    
                 }
-                
-               
+                //Add code to that above loop that tells a user if they put in text that isn't in the List.
+                if (standing == 0)
+                {
+                    Console.WriteLine("That is not a valid team name. Enter again:");
+                }
+                //Add code to that above loop that stops it from executing it from executing once a match has been found.
+                else
+                {
+                    isValid = true;
+                }
             }
-            Console.WriteLine("Index is: " + index);
+
+
+            Console.WriteLine("Processing... Index is: " + index + ". So...");
+            Console.WriteLine("Standing is: " + standing);
             Console.ReadLine();
 
+            // PART 5
+            // Create a List of strings that has at least two identical strings in the List.
+            List<string> tables = new List<string>() { "Reserved", "Vacant", "Taken", "Vacant", "Vacant", "Reserved", "Taken" };
+            Console.WriteLine("Welcome to Swell Taco! \nDue to COVID-19 we have a computerized check-in.\nIf you have a reservation type " +
+                "\"Reserved\".\nIf you are joining a party that is already here type \"Taken\".\nOtherwise type \"Vacant\".");
+            //Asks the user to select text to search for in the List.
+            Console.WriteLine("Enter selection:");
+            string selection = Console.ReadLine();
+            while (!(selection == "Vacant" || selection == "Reserved" || selection == "Taken"))
+            {
+                //Add code that tells a user if they put in text that isn't in the List.
+                Console.WriteLine("Please enter one of the 3 choices:");
+                selection = Console.ReadLine();
+            }
+            Console.WriteLine("The tables that match your selection are numbered:");
+            //Create a loop that iterates through the loop and then displays the indices of the array that contain matching text on the screen.
+            for (int i3 = 0; i3 < tables.Count; i3++)
+            {
+                if (selection == tables[i3])
+                {
+                    Console.WriteLine(i3);
+                }
+            }
+            Console.ReadLine();
+
+            // PART 6
+            // Create a List of strings that has at least two identical stirngs in the List.
+            List<string> names = new List<string>() { "Bob", "Jim", "Tiffany", "Bob", "Kat", "Mary", "Mary" };
+            List<string> repeatCheck = new List<string>();
+            Console.WriteLine("Class Roster:");
+            //Create a foreach loop that evaluates each item in the List, and displays a message showing the string and whether or not it has already appeared in the list.
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+                if (repeatCheck.Contains(name))
+                {
+                    Console.WriteLine("This name has been repeated. \nUse last name initial when referring to this student.");
+                }
+                else
+                {
+                    Console.WriteLine("This name has not been repeated.");
+                }
+                repeatCheck.Add(name);
+            }
+            Console.ReadLine();
         }
     }
 }
+
