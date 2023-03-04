c5b26afeee6c9dc4bdebf25ecc970f385938a078
Lilian Tan <lilytan6007@gmail.com>
2023-03-03 21:09:50 -0800

    Add Method class submission

---
+++ PART 2/Module4_MethodClassSubmission/Module4_MethodClassSubmission/MathMethod.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module4_MethodClassSubmission
+{
+    public class MathMethod
+    {
+        public void MathOp(int number1, int number2)
+        {
+           number1 = 7 * 8;
+           number2 = 8;
+
+          Console.WriteLine(number2);
+           
+        }
+    }
+}
---
+++ PART 2/Module4_MethodClassSubmission/Module4_MethodClassSubmission/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module4_MethodClassSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate the MathMethod class
+             MathMethod math1 = new MathMethod();
+            //Call the method in the class, passing in two numbers
+            Console.WriteLine(math1.MathOp(7, 9));
+            //Call the method in the class, specifying the parameters by name
+            Console.WriteLine(math1.MathOp("7"));
+            Console.ReadLine();
+        }
+    }
+}
