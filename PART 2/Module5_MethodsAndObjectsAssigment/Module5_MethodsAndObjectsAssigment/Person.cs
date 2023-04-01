d820eba62310e04bd0b0a49994e4ade8b70acad5
Lilian Tan <lilytan6007@gmail.com>
2023-03-31 19:04:15 -0700

    Add tutorial code

---
+++ PART 2/Module5_MethodsAndObjectsAssigment/Module5_MethodsAndObjectsAssigment/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module5_MethodsAndObjectsAssigment
+{
+   public class Person 
+    {
+        // Create a class called Person and give it to two properties, each of data types string 
+        //One called FirstName, the other LastName
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        // Give the class a void method called SayName() that takes no parameters and simply writes
+        // the person's full name to the console in the format of: "Name: [full name]"
+        public void SayName()
+        {
+            Console.WriteLine("Name: " + this.FirstName + " " + this.LastName);
+        }
+    }
+}
---
+++ PART 2/Module5_MethodsAndObjectsAssigment/Module5_MethodsAndObjectsAssigment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module5_MethodsAndObjectsAssigment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Inside of the Main method, instantiate and initialize an Employee subject with
+            //the first name "Sample" and the last name "Student"
+            Employee sam = new Employee() { FirstName = "Sample", LastName = "Student" };
+            //Call the superclass method SayName() on the Employee object
+            sam.SayName();
+            Console.ReadLine();
+        }
+    }
+}
