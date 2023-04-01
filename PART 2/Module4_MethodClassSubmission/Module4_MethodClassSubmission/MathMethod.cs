742236fb17fd6c3ab84e7bc180fa5d6ade7ec9f3
Lilian Tan <lilytan6007@gmail.com>
2023-03-31 17:28:20 -0700

    Add code

--- PART 2/Module4_MethodClassSubmission/Module4_MethodClassSubmission/MathMethod.cs
+++ PART 2/Module4_MethodClassSubmission/Module4_MethodClassSubmission/MathMethod.cs
@@ -8,13 +8,35 @@ namespace Module4_MethodClassSubmission
 {
     public class MathMethod
     {
-        public void MathOp(int number1, int number2)
-        {
-           number1 = 7 * 8;
-           number2 = 8;
+        //// First method that takes an integer
+        //public int MathOp(int number)
+        //{
+        //    return number + 9;
+        //}
+        //// Second method that takes a decimal
+        //public int MathOp(decimal number)
+        //{
+        //    // Convert the decimal to an int to perform math operation
+        //    int newnum = Convert.ToInt32(number);
+        //    return newnum * 30; 
+        //}
+        //// Third Method that takes a string, converts to an integer, performs a math operation and returns the result
+        //public int MathOp(string number)
+        //{
+        //    //Convert the string to an int to perform a math operation
+        //    int newnum = Convert.ToInt32(number);
+        //    return newnum / 2;
 
-          Console.WriteLine(number2);
-           
+        // create a void method that takes two integers as parameters. Have the method do a math operation on the first integer and display the second integer to the screen.
+        public int MathOp(int i, int j)
+        {
+            int result = i * j;
+            return result;
+        }
+        public int MathOp(string i, string j)
+        {
+            
         }
     }
 }
+
--- PART 2/Module4_MethodClassSubmission/Module4_MethodClassSubmission/Program.cs
+++ PART 2/Module4_MethodClassSubmission/Module4_MethodClassSubmission/Program.cs
@@ -10,12 +10,22 @@ namespace Module4_MethodClassSubmission
     {
         static void Main(string[] args)
         {
+            //// Instantiate the MathMethod class
+            // MathMethod math1 = new MathMethod();
+            ////Call the furst method in the class, passing in an integer parameter
+            //Console.WriteLine(math1.MathOp(9));
+            ////Call the second method in the class, passing in a decimal parameter
+            //Console.WriteLine(math1.MathOp(8.8m));
+            ////Call the third method with a string parameter
+            //Console.WriteLine(math1.MathOp("4"));
+            //Console.ReadLine();
+
             // Instantiate the MathMethod class
-             MathMethod math1 = new MathMethod();
-            //Call the method in the class, passing in two numbers
-            Console.WriteLine(math1.MathOp(7, 9));
+            MathMethod math1 = new MathMethod();
+            //Call the first method in the class, passing in two numbers
+            Console.WriteLine(math1.MathOp(9, 8));
             //Call the method in the class, specifying the parameters by name
-            Console.WriteLine(math1.MathOp("7"));
+            Console.WriteLine(math1.MathOp("i", "j"));
             Console.ReadLine();
         }
     }
