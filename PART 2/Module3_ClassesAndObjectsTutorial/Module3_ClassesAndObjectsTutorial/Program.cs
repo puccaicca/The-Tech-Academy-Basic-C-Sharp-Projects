4892804352ee88db3a5d3bcb9468c4c6203b2de9
Lilian Tan <lilytan6007@gmail.com>
2023-04-13 20:57:04 -0700

    add interfaces tutorial

--- PART 2/Module3_ClassesAndObjectsTutorial/Module3_ClassesAndObjectsTutorial/Program.cs
+++ PART 2/Module3_ClassesAndObjectsTutorial/Module3_ClassesAndObjectsTutorial/Program.cs
@@ -10,23 +10,23 @@ namespace Module3_ClassesAndObjectsTutorial // TwentyOne Game
     {
         static void Main(string[] args)
         {
-            TwentyOneGame game = new TwentyOneGame();
-            game.Players = new List<string>() { "Jesse", "Bill", "Joe" };
-            game.ListPlayers();
-            //game.Play();
-            Console.ReadLine();
-
-            //Deck deck = new Deck();
-            ////deck = Shuffle(deck); //first shuffle
-            //deck.Shuffle(3);
-
-            //foreach (Card card in deck.Cards)
-            //{
-            //    Console.WriteLine(card.Face + " of " + card.Suit);
-            //}
-            //Console.WriteLine(deck.Cards.Count);
-            ////Console.WriteLine("Times suffled: {0}", timesShuffled); // the {0} inputs the value from timesShuffled
+            //TwentyOneGame game = new TwentyOneGame();
+            //game.Players = new List<string>() { "Jesse", "Bill", "Joe" };
+            //game.ListPlayers();
+            ////game.Play();
             //Console.ReadLine();
+
+            Deck deck = new Deck();
+            ////deck = Shuffle(deck); //first shuffle
+            deck.Shuffle(3);
+
+            foreach (Card card in deck.Cards)
+            {
+                Console.WriteLine(card.Face + " of " + card.Suit);
+            }
+            Console.WriteLine(deck.Cards.Count);
+            ////Console.WriteLine("Times suffled: {0}", timesShuffled); // the {0} inputs the value from timesShuffled
+            Console.ReadLine();
         }
 
       
