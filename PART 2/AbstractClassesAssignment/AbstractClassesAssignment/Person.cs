16f38a257489eecce221bb22175061c6e1e042b8
Lilian Tan <lilytan6007@gmail.com>
2023-04-13 20:38:20 -0700

    abstract classes tutorial

---
+++ PART 2/AbstractClassesAssignment/AbstractClassesAssignment/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassesAssignment
+{
+    // Create an abstract class "Person: with two string properties: firstName and lastName
+    public abstract class Person
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        //Give it the method SayName()
+        public abstract void SayName();
+    }
+}
---
+++ PART 2/AbstractClassesAssignment/AbstractClassesAssignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassesAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //In the Main method, instantiate an Employee object
+            Employee sam = new Employee() { firstName = "Sample", lastName = " Student" };
+            //Call the SayName method on the object
+            sam.SayName();
+            Console.ReadLine();
+        }
+    }
+}
