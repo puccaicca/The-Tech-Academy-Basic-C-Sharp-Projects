b3d181ebcb1300491434ce094909b96a3b8353bb
Lilian Tan <lilytan6007@gmail.com>
2023-04-27 21:04:05 -0700

    Add code to compare two employee objects

---
+++ PART 2/Operators Assignment/Operators Assignment/Employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators_Assignment
+{
+    public class Employee : Person
+    {
+        public int ID { get; set; }
+
+        public static bool operator == (Employee employee, Employee employee2)
+        {
+            if (employee.ID == employee2.ID)
+
+                return true;
+            else
+                return false;
+        }
+
+        public static bool operator != (Employee employee, Employee employee2)
+        {
+            return employee.ID != employee2.ID;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is null) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false; //optional. depends on logic
+            return this.ID.Equals(((Employee)obj).ID);
+        }
+    }
+}
---
+++ PART 2/Operators Assignment/Operators Assignment/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee example = new Employee();
+
+            example.FirstName = "Lilian";
+            example.LastName = "Tan";
+            example.ID = 24923;
+
+            Employee example2 = new Employee();
+            example2.FirstName = "John";
+            example2.LastName = "Wick";
+            example2.ID = 39292;
+
+            Console.WriteLine("Employee 1 Information \n \n" + "ID =  " + example.ID + "\n" + "First Name =  "+ example.FirstName + "\n" + "Last Name =  "+ example.LastName + "\n \n");
+            Console.WriteLine("Employee 2 Information \n \n" + "ID =  " + example.ID + "\n" + "First Name =  " + example2.FirstName + "\n" + "Last Name =  " + example2.LastName + "\n \n");
+
+
+           Console.WriteLine("--------- DUPLICATE EMPLOYEE ID ----------- \n");
+
+            if (example == example2)
+            {
+                Console.WriteLine("Dublicate ID found");
+            }
+            else
+            {
+                Console.WriteLine("NO Dublicate ID found");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
