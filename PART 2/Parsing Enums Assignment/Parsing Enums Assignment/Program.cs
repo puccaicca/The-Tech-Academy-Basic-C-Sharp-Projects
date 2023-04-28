ec2eade7f5be972939d8a973f402d03836d22020
Lilian Tan <lilytan6007@gmail.com>
2023-04-27 23:00:26 -0700

    Add parsing enums assignment

---
+++ PART 2/Parsing Enums Assignment/Parsing Enums Assignment/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parsing_Enums_Assignment
+{
+    class Program
+    {
+        //Create an enum for the days of the week
+        public enum DaysOfTheWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+        static void Main(string[] args)
+        {
+            bool isvalid = false;
+            while(!isvalid)
+            {
+                try
+                {
+                    //Prompt the user to enter the current day of the week.
+                    Console.WriteLine("Enter the current day of the week");
+                    string dayinput = Console.ReadLine();
+                    //Assign the value to a variable of that enum data type you just created.
+                    DaysOfTheWeek day = (DaysOfTheWeek)Enum.Parse(typeof(DaysOfTheWeek), dayinput);
+                    Console.WriteLine("Have a nice " + day);
+                    Console.ReadLine();
+                    isvalid = true;
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine("Please enter an actual day of the week");
+                }
+            }
+        }
+    }
+}
