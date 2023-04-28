3c2723b85c880f6b6802ce4cd54ba8b6fd1d4064
Lilian Tan <lilytan6007@gmail.com>
2023-04-27 22:07:57 -0700

    Add parmaters using generics assignment

---
+++ PART 2/Parameters (Generics) Assignment/Parameters (Generics) Assignment/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parameters__Generics__Assignment
+{
+    public class Employee<T>
+    {
+        public int ID { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public List<T> Things { get; set; }
+    }
+}
---
+++ PART 2/Parameters (Generics) Assignment/Parameters (Generics) Assignment/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parameters__Generics__Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> emp = new Employee<string>();
+            emp.Things = new List<string>();
+
+            emp.Things.Add("Lilian");
+            emp.Things.Add("Karen");
+            emp.Things.Add("Wendy");
+
+            foreach (string employee in emp.Things)
+            {
+                Console.WriteLine(employee);
+            }
+
+            Employee<int> emp1 = new Employee<int>();
+            emp1.Things = new List<int>();
+
+            emp1.Things.Add(1);
+            emp1.Things.Add(2);
+            emp1.Things.Add(3);
+
+            // create a loop that prints all of Things to the Console
+            foreach (int employee in emp1.Things)
+            {
+                Console.WriteLine(employee);
+            }
+
+            //foreach (string emplist in emp.Things)
+            //{
+            //    Console.WriteLine(emplist);
+            //}
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to shut down ....");
+            Console.ReadLine();
+        }
+    }
+}
