aad33302f2387a87ef5eeb9c717956c1cc938020
Lilian Tan <lilytan6007@gmail.com>
2023-04-27 22:52:38 -0700

    Add enums tutorial

--- PART 2/Module3_ClassesAndObjectsTutorial/Module3_ClassesAndObjectsTutorial/Program.cs
+++ PART 2/Module3_ClassesAndObjectsTutorial/Module3_ClassesAndObjectsTutorial/Program.cs
@@ -16,28 +16,30 @@ namespace Module3_ClassesAndObjectsTutorial // TwentyOne Game
             ////game.Play();
             //Console.ReadLine();
 
-            Game game = new TwentyOneGame();
-            game.Players = new List<Player>();
-            Player player = new Player();
-            player.Name = "Jesse";
-            game += player;
-            game -= player;
+            //Game game = new TwentyOneGame();
+            //game.Players = new List<Player>();
+            //Player player = new Player();
+            //player.Name = "Jesse";
+            //game += player;
+            //game -= player;
+           
 
-
+            
+            
             Deck deck = new Deck();
-            ////deck = Shuffle(deck); //first shuffle
             deck.Shuffle(3);
 
             foreach (Card card in deck.Cards)
-            {
-                Console.WriteLine(card.Face + " of " + card.Suit);
+            { 
+            Console.WriteLine(card.Face + " of " + card.Suit);
             }
             Console.WriteLine(deck.Cards.Count);
             ////Console.WriteLine("Times suffled: {0}", timesShuffled); // the {0} inputs the value from timesShuffled
             Console.ReadLine();
         }
 
-      
+        
+        
 
         //public static Deck Shuffle(Deck deck, int times)
         //{
