d59a00966f618b4cf89e4764a015b9e3dd118663
Lilian Tan <lilytan6007@gmail.com>
2023-05-30 19:32:48 -0700

    Add lambda expression assignment

---
+++ PART 2/Lambda Expression Assignment/Lambda Expression Assignment/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda_Expression_Assignment
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+    }
+}
---
+++ PART 2/Lambda Expression Assignment/Lambda Expression Assignment/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda_Expression_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //In the Main() method, create a list of at least 10 employees.
+            //At least two employees should have the first name "Joe".
+            List<Employee> campus = new List<Employee>()
+            {
+                new Employee() {ID = 1, firstName = "Joe", lastName = "Schmo"},
+                new Employee() {ID = 2, firstName = "Joe", lastName = "Thomas"},
+                new Employee() {ID = 3, firstName = "Jimbob", lastName = "Coot"},
+                new Employee() {ID = 4, firstName = "Lilian", lastName = "Tan"},
+                new Employee() {ID = 5, firstName = "Vivien", lastName = "Lamb"},
+                new Employee() {ID = 6, firstName = "Nicki", lastName = "Minaj"},
+                new Employee() {ID = 7, firstName = "Seokjin", lastName = "Kim"},
+                new Employee() {ID = 8, firstName = "Jungkook", lastName = "Jeon"},
+                new Employee() {ID = 9, firstName = "Namjoon", lastName = "Kim"},
+                new Employee() {ID = 10, firstName = "Jennifer", lastName = "Huh"}
+            };
+            //Using a foreach loop, create a new list of all employees with the first name "Joe"
+            List<Employee> joes = new List<Employee>();
+            foreach (Employee employee in campus)
+            {
+                if (employee.firstName == "Joe")
+                {
+                    joes.Add(employee);
+                }
+            }
+            //Do the same thing again, but this time with a lambda expression
+            List<Employee> joes2 = campus.Where(x => x.firstName == "Joe").ToList();
+            //Using a lambda expression, make a list of all employees with an Id number greater than 5
+            List<Employee> bigId = campus.Where(x => x.ID > 5).ToList();
+            Console.ReadLine();
+        }
+    }
+}
