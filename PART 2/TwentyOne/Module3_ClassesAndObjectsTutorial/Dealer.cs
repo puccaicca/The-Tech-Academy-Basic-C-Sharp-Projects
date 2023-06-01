8e2c7311326b222449f0e2d45127e0591e5c760d
Lilian Tan <lilytan6007@gmail.com>
2023-05-31 22:09:38 -0700

    Add dateTime excerise

--- PART 2/TwentyOne/Module3_ClassesAndObjectsTutorial/Dealer.cs
+++ PART 2/TwentyOne/Module3_ClassesAndObjectsTutorial/Dealer.cs
@@ -20,6 +20,7 @@ namespace TwentyOne
             Console.WriteLine(card);
             using (StreamWriter file = new StreamWriter(@"C:\Users\heart\Logs\log.txt", true))
             {
+                file.WriteLine(DateTime.Now);
                 file.WriteLine(card);
             }
             Deck.Cards.RemoveAt(0);
--- PART 2/TwentyOne/Module3_ClassesAndObjectsTutorial/Program.cs
+++ PART 2/TwentyOne/Module3_ClassesAndObjectsTutorial/Program.cs
@@ -11,6 +11,12 @@ namespace TwentyOne // TwentyOne Game
     {
         static void Main(string[] args)
         {
+            DateTime dyearOfBirth = new DateTime(1995, 5, 23, 8, 32, 45);
+            DateTime yearOfGraduation = new DateTime(2013, 6, 1, 16, 34, 22);
+
+            TimeSpan ageAtGraduation = yearOfGraduation - dyearOfBirth;
+            
+
             Console.WriteLine("Welcome to the Grand Hotel and Casino. Let's start by telling me your name.");
             string playerName = Console.ReadLine();
             Console.WriteLine("And how much money did you bring today?");
