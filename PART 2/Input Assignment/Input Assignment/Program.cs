6a4dbef79140b41707ef91006bb8fa768c6ae7fd
Lilian Tan <lilytan6007@gmail.com>
2023-05-31 23:07:46 -0700

    Add input assignment

---
+++ PART 2/Input Assignment/Input Assignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Input_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's your favorite number?");
+            string number = Console.ReadLine();
+            File.WriteAllText(@"C:\Users\heart\Logs\log2.txt", number);
+          
+        }
+    }
+}
