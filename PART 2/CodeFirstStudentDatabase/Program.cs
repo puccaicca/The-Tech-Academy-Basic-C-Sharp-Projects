58f5d9f0e902fbe01cc1a79bfb26fa5552533e81
Lilian Tan <lilytan6007@gmail.com>
2023-06-27 18:24:56 -0700

    Add Final Assignment

---
+++ PART 2/CodeFirstStudentDatabase/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace CodeFirstStudentDatabase
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            using (var ctx = new SchoolContext())
+            {
+                var student = new Student() { StudentName = "Bill" };
+
+                ctx.Students.Add(student);
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
---
+++ PART 2/CodeFirstStudentDatabase/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstStudentDatabase
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public required string StudentName { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public byte[]? Photo { get; set; }
+        public decimal Height { get; set; }
+        public float Weight { get; set; }
+
+        public int? GradeId { get; set; }
+
+
+    }
+}
